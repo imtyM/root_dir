fda4271e8d15e6c0063d7a804d9fd67d04b5d652
Imtiaz Mukadam <imtiaz@LAPTOP-ILHAGTI8.localdomain>
2022-07-22 17:02:43 +0200

    chore(init): init svelte kit app

---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Prompt from "$lib/Components/Prompt.svelte"
+
+  let value = ""
+  let history = ""
+
+  const submit = () => {
+    history = history + '\n' + value
+    value = ''
+  }
+</script>
+
+<form on:submit|preventDefault={submit}>
+  <textarea readonly bind:value={history}/>
+  <Prompt bind:value />
+  <input type="submit" hidden/>
+</form>
