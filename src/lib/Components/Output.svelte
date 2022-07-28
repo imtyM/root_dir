97fa991267d202934d416e2410f80a7c065f2a5a
Imtiaz Mukadam <imtiaz@LAPTOP-ILHAGTI8.localdomain>
2022-07-28 22:22:11 +0200

    chore(cli): Rename History component to Outputs
    In a cli, I think outputs makes more sense.

---
+++ src/lib/Components/Output.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let elements
+  $: console.log({elements})
+</script>
+
+{#each elements as element}
+  <svelte:component this={element?.component} {...element.props}/>
+{/each}
+
