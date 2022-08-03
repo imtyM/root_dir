cb4b66cf5f4dadc48acc50ec946f4542e94e25e3
imtyM <m.imtiazmukadam@gmail.com>
2022-08-03 19:36:23 +0200

    chore(term): add trivial who commands

---
+++ src/lib/Components/Commands/WhoAmI.svelte
@@ -0,0 +1,10 @@
+<script context="module">
+  export const meta = {
+      name: 'Who am I',
+      cmd: 'whoami'
+    };
+</script>
+
+<div>
+  <p>I have no idea who you are.</p>
+</div>
---
+++ src/lib/Components/Commands/WhoAreYou.svelte
@@ -0,0 +1,13 @@
+<script context="module">
+  export const meta = {
+      name: 'Who are you',
+      cmd: 'whoareyou'
+    };
+</script>
+
+<div>
+  <p>My name is Imtiaz</p>
+  <p>I am a software engineer</p>
+  <p>I am often in a terminal, and now I kind of made my own</p>
+  <p>I hope you're enjoying it!</p>
+</div>
