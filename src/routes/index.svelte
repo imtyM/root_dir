8125952d5815ef183b6118896939625652b2c60e
imtyM <m.imtiazmukadam@gmail.com>
2022-09-08 20:51:35 +0200

    chore(app): abstract terminal into its own component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,33 +1,5 @@
 <script lang="ts">
-  import type { SvelteComponent } from "svelte"
-  import Output from "$lib/Components/Output.svelte"
-  import Input from "$lib/Components/Input.svelte"
-
-  import { processCmd } from "$lib/processCmd"
-  import { valueIsClear } from '$lib/valueIsClear'
-
-  type Result = {
-    component: typeof SvelteComponent|null,
-    props?: {
-      cmd?: string|null
-    }
-  }
-
-  let value = ""
-  let elements: Result[] = []
-
-  const submit = () => {
-    if (valueIsClear(value)) {
-      elements = []
-    } else {
-      elements = [...elements, processCmd(value)]
-    }
-    value = ''
-  }
+  import Terminal from '$lib/Components/Terminal.svelte'
 </script>
 
-<form on:submit|preventDefault={submit}>
-  <Output elements={elements} />
-  <Input bind:value />
-  <input type="submit" hidden/>
-</form>
+<Terminal />
